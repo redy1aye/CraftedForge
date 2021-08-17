ccfee092acb8555470f33d32816328a986e2aa20
Redy1aye <86603844+Redy1aye@users.noreply.github.com>
2021-08-17 08:59:22 +0500

    Add files via upload

---
+++ src/main/java/dev/redy1aye/crafted/Redy1ayeMod.java
@@ -0,0 +1,11 @@
+package dev.redy1aye.crafted;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("crafted")
+public class Redy1ayeMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+}
